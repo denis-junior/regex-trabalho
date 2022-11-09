01e1515a29bbe6fd2f2ffc6da01c6efe5cac6969
Denis Junior <denis_jr2001@hotmail.com>
2022-11-09 16:45:10 -0300

    phone number regex ok

---
+++ formatPhoneNumber.js
@@ -0,0 +1,5 @@
+const regexPhoneNumber = /^(\d{2})(\d{5})(\d{4})$/
+
+const formatPhoneNumber = phoneNumber => phoneNumber.replace(regexPhoneNumber, "($1) $2-$3")
+
+console.log(formatPhoneNumber("91989359625"))
